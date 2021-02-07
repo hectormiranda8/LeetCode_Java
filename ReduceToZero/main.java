83e40eccebbb9d537a920b2f72307e68587f910e
HectorMiranda <hector.miranda8@upr.edu>
2021-02-07 02:53:39 -0400

    Solved Steps to reduce a number to zero

---
+++ ReduceToZero/main.java
@@ -0,0 +1,23 @@
+class ReduceToZero {
+    public static void main(String[] args){
+        System.out.println(numberOfSteps(14));
+        System.out.println(numberOfSteps(8));
+        System.out.println(numberOfSteps(123));
+    }
+
+    public static int numberOfSteps (int num) {
+        int steps = 0;
+
+        while(num != 0){
+            if(num % 2 == 0){
+                num /= 2;
+            }
+            else {
+                num -= 1;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+}
